1e93cc65ca9a7d342372c69764c138a6261ee6b2
anshuldhuryani <anshul.dhuryani@gmail.com>
2023-03-01 13:04:55 +0530

    Final Commit

---
+++ CSharp_Inheritance_Polymorphism_Fundamentals/ConstructorChainingExample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Inheritance_Polymorphism_Fundamentals
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        public Person()
+        {
+            this.name = "Sarah Bowling";
+            this.age = 23;
+        }
+        public void show()
+        {
+            Console.WriteLine("Name: {0}, Age: {1}", this.name, this.age);
+        }
+    }
+
+    class Employee : Person
+    {
+        private string designation;
+        private float salary;
+        public Employee()
+        {
+            this.designation = "Manager";
+            this.salary = 50000;
+        }
+        public new void show()
+        {
+            base.show();
+            Console.WriteLine("Designation: {0}, Salary: {1}", this.designation, this.salary);
+        }
+    }
+
+    class ConstructorChainingExample
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.show();
+        }
+    }
+}
--- CSharp_Inheritance_Polymorphism_Fundamentals/InheritanceExample.cs
+++ CSharp_Inheritance_Polymorphism_Fundamentals/InheritanceExample.cs
@@ -3,15 +3,15 @@ using System;
 
 namespace CSharp_Inheritance_Polymorphism_Fundamentals
 {
-    class Program
+    class InheritanceExample
     {
-        static void Main(string[] args)
+       /* static void Main(string[] args)
         {
             Result info = new Result();
             info.acceptDetails();
             info.displayDetails();
             info.calculateResult();
 
-        }
+        }*/
     }
 }
